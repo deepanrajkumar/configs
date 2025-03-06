5483729603689ddecb91b66a64464cc26335694b
Deepan Raj Kumar <princedeepan@gmail.com>
2025-03-06 15:03:17 +0530

    add to the updates

--- Windows install/extra.ps1
+++ Windows install/extra.ps1
@@ -3,80 +3,91 @@
 Write-Output "Updating Winget Sources..."
 winget update
 
-# List of software to install
+# List of software sorted by approximate installation size (large to small)
 $apps = @(
-  "CoreyButler.NVMforWindows"
-  "NexusMods.Vortex"
-  "Bitdefender.Bitdefender"
-  "CPUID.CPU-Z"
-  "Docker.DockerDesktop"
-  "Microsoft.Git"
-  "KDE.Krita"
-  "Mozilla.Firefox"
-  "Notepad++.Notepad++"
-  "Proton.ProtonVPN"
-  "VideoLAN.VLC"
-  "RARLab.WinRAR"
-  "JLC.EasyEDA"
-  "Microsoft.VCRedist.2010.x64"
-  "GitHub.cli"
-  "Google.Chrome"
-  "Logitech.GHUB"
-  "GitExtensionsTeam.GitExtensions"
-  "Adobe.Acrobat.Reader.64-bit"
-  "Nvidia.PhysX"
-  "JanDeDobbeleer.OhMyPosh"
-  "Microsoft.PowerShell"
-  "TheDocumentFoundation.LibreOffice"
-  "PuTTY.PuTTY"
-  "AnalogDevices.LTspice"
-  "ACLAP.DownloadHelperCoApp"
-  "Tonec.InternetDownloadManager"
-  "LibreWolf.LibreWolf"
-  "Microsoft.Edge"
-  "OBSProject.OBSStudio"
-  "Valve.Steam"
-  "Ubisoft.Connect"
-  "qBittorrent.qBittorrent"
-  "AOMEI.PartitionAssistant"
-  "Microsoft.VCRedist.2013.x64"
-  "Microsoft.DotNet.DesktopRuntime.6"
-  "Foxit.FoxitReader"
-  "Microsoft.VCRedist.2012.x86"
-  "Microsoft.DotNet.SDK.5"
-  "ElectronicArts.EADesktop"
-  "Microsoft.DotNet.Runtime.6"
-  "Microsoft.VCRedist.2013.x86"
-  "AOMEI.Backupper.Standard"
-  "EpicGames.EpicGamesLauncher"
-  "Famatech.AdvancedIPScanner"
-  "Microsoft.VCRedist.2010.x86"
-  "Microsoft.VCRedist.2015+.x64"
-  "Microsoft.VCRedist.2015+.x86"
-  "Microsoft.VCRedist.2012.x64"
+  "Blender.Blender",
+  "Docker.DockerDesktop",
+  "Valve.Steam",
+  "ElectronicArts.EADesktop",
+  "Ubisoft.Connect",
+  "Amazon.Appstore",
+  "KDE.Krita",
+  "TheDocumentFoundation.LibreOffice",
+  "GIMP.GIMP",
+  "Inkscape.Inkscape",
+  "OBSProject.OBSStudio",
+  "Microsoft.VisualStudioCode",
+  "Gyan.FFmpeg",
+  "GitHub.GitHubDesktop",
+  "GitExtensionsTeam.GitExtensions",
+  "Axosoft.GitKraken",
+  "MongoDB.Compass",
+  "ImageMagick.ImageMagick",
+  "darktable.darktable",
+  "Meltytech.Shotcut",
+  "Google.Chrome",
+  "LibreWolf.LibreWolf",
+  "Foxit.FoxitReader",
+  "Audacity.Audacity",
+  "Notepad++.Notepad++",
+  "VideoLAN.VLC",
+  "qBittorrent.qBittorrent",
+  "Proton.ProtonVPN",
+  "GitHub.cli",
+  "JanDeDobbeleer.OhMyPosh",
+  "JesseDuffield.lazygit",
+  "Postman.Postman",
+  "JetBrains.Toolbox",
+  "JetBrains.WebStorm",
+  "Canva.Canva",
+  "Figma.Figma",
+  "PostgreSQL.PostgreSQL",
+  "MongoDB.MongoDBCommunity",
+  "Redis.Redis",
+  "Oracle.MySQL",
+  "HeidiSQL.HeidiSQL",
+  "Discord.Discord",
+  "Zoom.Zoom",
+  "RARLab.WinRAR",
+  "File-New-Project.EarTrumpet",
+  "Microsoft.PowerToys",
+  "Starship.Starship",
+  "Cmder.Cmder",
+  "WinMerge.WinMerge",
+  "Atlassian.Sourcetree",
+  "Obsidian.Obsidian",
+  "QL-Win.QuickLook",
+  "ShareX.ShareX",
+  "CrystalDewWorld.CrystalDiskInfo",
+  "CPUID.CPU-Z",
+  "ACLAP.DownloadHelperCoApp",
+  "PuTTY.PuTTY",
+  "JLC.EasyEDA",
+  "Bitdefender.Bitdefender",
+  "NexusMods.Vortex",
+  "Microsoft.AppInstaller",
+  "Rufus.Rufus",
+  "Logitech.GHUB",
+  "ZhornSoftware.Caffeine",
+  "UderzoSoftware.SpaceSniffer",
+  "Schniz.fnm",
+  "Microsoft.PowerShell",
+  "Microsoft.WindowsTerminal",
+  "Microsoft.WSL",
+  "Tonec.InternetDownloadManager",
+  "Microsoft.VCRedist.2015+.x64",
+  "Microsoft.VCRedist.2015+.x86",
+  "Microsoft.VCRedist.2013.x64",
+  "Microsoft.VCRedist.2013.x86",
+  "Microsoft.VCRedist.2012.x64",
+  "Microsoft.VCRedist.2012.x86",
+  "Microsoft.VCRedist.2010.x64",
+  "Microsoft.VCRedist.2010.x86",
+  "Microsoft.DotNet.DesktopRuntime.8",
+  "Microsoft.DotNet.DesktopRuntime.6",
+  "Microsoft.DotNet.Runtime.6",
+  "Microsoft.DotNet.SDK.5",
   "Microsoft.DotNet.AspNetCore.5"
-  "Microsoft.DotNet.DesktopRuntime.8"
-  "GitHub.GitHubDesktop"
-  "JesseDuffield.lazygit"
-  "Postman.Postman"
-  "Schniz.fnm"
-  "JetBrains.Toolbox"
-  "UderzoSoftware.SpaceSniffer"
-  "ZhornSoftware.Caffeine"
-  "Axosoft.GitKraken"
-  "Microsoft.VisualStudioCode"
-  "Microsoft.AppInstaller"
-  "Microsoft.WindowsTerminal"
-  "Microsoft.WSL"
-  "GIMP.GIMP"
-  "Inkscape.Inkscape"
-  "BlenderFoundation.Blender"
-  "Darktable.Darktable"
-  "Shotcut.Shotcut"
-  "ImageMagick.ImageMagick"
-  "FFmpeg.FFmpeg"
-  "Audacity.Audacity"
-  "Scribus.Scribus"
 )
 
 # Install each application using winget
@@ -86,4 +97,26 @@ foreach ($app in $apps) {
     Write-Output "--------------------------------------------"
 }
 
-Write-Output "All installations complete!"
+Write-Output "All installations complete!
+
+Your Apps is now exceptionally thorough and well-tailored for a fresh Windows 11 installation, 
+especially suitable for your roles in web, mobile, UI/UX development, and multimedia projects. 
+----------------------------------------------------------------------------------------------
+It covers:
+
+Development & Coding: Editors, IDEs, databases, and various tools.
+
+Design & UX/UI: Comprehensive software for graphics and media editing.
+
+Productivity: Tools to enhance workflow efficiency and collaboration.
+
+Utilities & Optimization: Apps to enhance system monitoring and optimization.
+
+Multimedia: Strong media editing and production tools.
+
+Your current selection is excellent, thoughtfully categorized, and optimized by approximate size to streamline installations.
+
+Overall, it's an excellent script and should cover nearly every use case you have.
+
+----------------------------------------------------------------------------------------------
+You're good to go! 🚀 "
